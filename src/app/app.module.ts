bc53fc0e9b1d27a511e28d638bbf0cac34534651
DutchTechie <rkock127@gmail.com>
2020-02-20 00:46:26 +0100

    Create product catalog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ProductCatalogComponent } from './products/product-catalog/product-catalog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ProductCatalogComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/products/product-catalog/product-catalog.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../product.model'
+
+@Component({
+  selector: 'app-product-catalog',
+  templateUrl: './product-catalog.component.html',
+  styleUrls: ['./product-catalog.component.css']
+})
+export class ProductCatalogComponent implements OnInit {
+  products : Product[] = [
+    new Product("shoe", "What can I say? It's a bloody shoe.", "no path")
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
