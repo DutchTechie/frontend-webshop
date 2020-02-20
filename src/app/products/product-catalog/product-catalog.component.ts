581bfb4ce26a094b99841474234e7f26802bb419
DutchTechie <rkock127@gmail.com>
2020-02-20 11:02:29 +0100

    create on click list item event

--- src/app/products/product-catalog/product-catalog.component.ts
+++ src/app/products/product-catalog/product-catalog.component.ts
@@ -9,8 +9,13 @@ import { Product } from '../product.model'
 export class ProductCatalogComponent implements OnInit {
   products : Product[] = [
     new Product("shoe", "What can I say? It's a bloody shoe.", "no path"),
-    new Product("shoe", "What can I say? It's a bloody shoe.", "no path")
+    new Product("shoe 2", "What can I say? It's a bloody shoe.", "no path")
   ];
+
+  updateDetails(event) {
+    console.log(event)
+    alert(event)
+  }
 
   constructor() { }
 
